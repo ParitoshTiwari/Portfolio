aab49537023edc2feecadf43b89635ad13784a3b
Paritosh Tiwari <Paritosh1.tiwari@gmail.com>
2023-08-24 22:11:16 -0500

    Changes in portfolio

--- MyPortfolio/MyPortfolio/Program.cs
+++ MyPortfolio/MyPortfolio/Program.cs
@@ -1,6 +1,3 @@
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyPortfolio;
@@ -10,12 +7,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services
-    .AddBlazorise(options =>
-    {
-        options.Immediate = true;
-    })
-    .AddBootstrapProviders()
-    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
